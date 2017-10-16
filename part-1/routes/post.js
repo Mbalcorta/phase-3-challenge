2f1c922c887e94b271cde14fb6bb38417bcc79e4
Mateo Balcorta <misterrattail@gmail.com>
2017-10-16 12:57:05 -0700

    Add post route.

---
+++ part-1/routes/post.js
@@ -0,0 +1,8 @@
+const router = require('express').Router()
+
+router.post('/api/array/concat', function (request, response) {
+  const requestObject = request.body
+  //start function call here to concat
+})
+
+module.exports = router
