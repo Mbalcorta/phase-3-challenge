62fedb275e1155f9799edb507a8cb17f20ad8c4f
Mateo Balcorta <misterrattail@gmail.com>
2017-10-16 11:38:24 -0700

    add query function to route to handle day value promise.

--- part-1/routes/get.js
+++ part-1/routes/get.js
@@ -1,7 +1,16 @@
 const router = require('express').Router()
+const dayValue = require('../js/getFunc')
 
-router.get('/', function (req, res) {
-  res.send('I work!')
+router.get('/api/days/:day', function (req, res) {
+  let query = req.params.day
+  dayValue(query)
+  .then(response => {
+    res.send(response)
+  })
+  .catch((error) => {
+    console.log(error)
+    res.send(error)
+  })
 })
 
 module.exports = router
