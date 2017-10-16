ceb3489c3dba3c50ac11c5ab5c947ab61cdb56c1
Mateo Balcorta <misterrattail@gmail.com>
2017-10-16 12:56:29 -0700

    Add body-parser.

--- part-1/app.js
+++ part-1/app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.json())
 app.use(require('./routes/get'))
+app.use(require('./routes/post'))
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
