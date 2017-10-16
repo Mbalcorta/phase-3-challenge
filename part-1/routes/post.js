67ace312f1c056510b036f5ecfcf4711e67bbed2
Mateo Balcorta <misterrattail@gmail.com>
2017-10-16 13:26:54 -0700

    Set headers content-type, status code and sent response.

--- part-1/routes/post.js
+++ part-1/routes/post.js
@@ -1,8 +1,17 @@
 const router = require('express').Router()
+const concat = require('../js/postFunc')
 
 router.post('/api/array/concat', function (request, response) {
   const requestObject = request.body
-  //start function call here to concat
+  concat(requestObject)
+  .then(concatArray => {
+    response.set('Content-Type', 'application/json')
+    response.status(200).send(concatArray)
+  })
+  .catch((error) => {
+    response.set('Content-Type', 'application/json')
+    response.status(400).send(error)
+  })
 })
 
 module.exports = router
