71419eeed1b92325e418f995e78a2c77473046f1
Mateo Balcorta <misterrattail@gmail.com>
2017-10-17 16:02:53 -0700

    Add functionality to run first node argument real-shoppers.

--- part-2/store.js
+++ part-2/store.js
@@ -1,1 +1,17 @@
 #!/usr/bin/env node
+
+const {getActiveShoppers} = require('./database/database.js')
+const firstArgument = process.argv.slice(2)[0];
+// const taskString = process.argv.slice(3).join(' ');
+
+switch(firstArgument){
+  case 'real-shoppers' :
+    getActiveShoppers()
+    .then(data => {
+      data.forEach(eachElement => {
+        process.stdout.write(`Shopper name: ${eachElement.name}\n`)
+      })
+    })
+    .catch(console.error)
+    break;
+}
