104823f4af76144f761afc4802bd539b6438de85
Mateo Balcorta <misterrattail@gmail.com>
2017-10-16 21:44:22 -0700

    Removed modal function to close window when clicked outside of modal box.

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -1,6 +1,7 @@
 const cartDetails = {
   cartCount: 0
 }
+
 const displayCartTotal = () => {
   document.getElementById('cart-item-count').innerHTML = `(${cartDetails.cartCount})`
 }
@@ -25,27 +26,17 @@ document.getElementById('clear').onclick = clearCart
 // Get the modal
 var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks on the button, open the modal
 document.getElementsByTagName("button")[0].onclick = function() {
     modal.style.display = "block";
 }
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
 }
 
 
