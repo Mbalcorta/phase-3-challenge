f0d765e38823d7947fac97152cbdc09d1c0260d9
Mateo Balcorta <misterrattail@gmail.com>
2017-10-16 21:36:41 -0700

    Add modal js functionality and clear button to update cart number to zero.

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -1,15 +1,51 @@
 const cartDetails = {
   cartCount: 0
 }
-
-const updateCartCount = () => {
-  cartDetails.cartCount++
+const displayCartTotal = () => {
   document.getElementById('cart-item-count').innerHTML = `(${cartDetails.cartCount})`
 }
 
+const updateCartCount = () => {
+  cartDetails.cartCount++
+  displayCartTotal()
+}
+
+const clearCart = () => {
+  cartDetails.cartCount = 0
+  displayCartTotal()
+}
+
 const addToCartButtons = document.getElementsByTagName("button")
-for(var i = 1; i < addToCartButtons.length; i++){
+for(var i = 1; i < addToCartButtons.length-1; i++){
   addToCartButtons[i].onclick = updateCartCount
+}
+
+document.getElementById('clear').onclick = clearCart
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+document.getElementsByTagName("button")[0].onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
 
 
