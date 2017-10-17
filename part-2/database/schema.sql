6ab4b8f3978316876582142ca79d0e01c06cc0a1
Mateo Balcorta <misterrattail@gmail.com>
2017-10-17 15:15:09 -0700

    Add join table orderItems table and modified orders join table.

--- part-2/database/schema.sql
+++ part-2/database/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE grocery_items (
   itemID SERIAL PRIMARY KEY,
-  itemName VARCHAR(255) NOT NULL,
+  itemName VARCHAR(255) NOT NULL UNIQUE,
   price decimal(4, 2) NOT NULL,
   section VARCHAR(255) NOT NULL
 );
@@ -12,6 +12,10 @@ CREATE TABLE shoppers (
 
 CREATE TABLE orders (
   orderID SERIAL PRIMARY KEY,
-  shopperId INTEGER REFERENCES shoppers NOT NULL,
+  shopperId INTEGER REFERENCES shoppers NOT NULL
+);
+
+CREATE TABLE orderItems (
+  orderID INTEGER REFERENCES orders NOT NULL,
   groceryItem INTEGER REFERENCES grocery_items(itemID) NOT NULL
 );
