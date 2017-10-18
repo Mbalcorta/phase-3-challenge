52d26c600253857160e251607786f0bb690f1e1b
Mateo Balcorta <misterrattail@gmail.com>
2017-10-17 19:28:49 -0700

    Add query for product-list.

--- part-2/database/database.js
+++ part-2/database/database.js
@@ -16,4 +16,8 @@ const shoppersOrders = (param) => {
   return database.any(`SELECT i.orderid, SUM(g.price) AS total_cost FROM orders o JOIN orderitems i ON(i.orderid = o.orderid) JOIN grocery_items g ON(g.itemid = i.groceryitem)WHERE o.shopperid = ${param} GROUP BY i.orderid`)
 }
 
-module.exports = {getActiveShoppers, shoppersOrders}
+const productList = (param) => {
+  return database.any(`SELECT i.itemname, i.section FROM grocery_items i WHERE section='${param}'`)
+}
+
+module.exports = {getActiveShoppers, shoppersOrders, productList}
