eb7e684954567140a75ce29b2d02652bdf67b441
Mateo Balcorta <misterrattail@gmail.com>
2017-10-17 17:02:07 -0700

    Add query to get active shoppers.

--- part-2/database/database.js
+++ part-2/database/database.js
@@ -9,7 +9,7 @@ const connection = {
 const database = pgp(connection)
 
 const getActiveShoppers = () => {
-  return database.any('SELECT * FROM shoppers')
+  return database.any('SELECT count(o.shopperid) AS orders, s.name FROM orders o JOIN shoppers s ON(s.shopperid = o.shopperid) GROUP BY o.shopperid, s.name')
 }
 
 module.exports = {getActiveShoppers}
