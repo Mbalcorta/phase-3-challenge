733fc00f7d5f9b05f37102846c7d0d3d7c9f5ca5
Mateo Balcorta <misterrattail@gmail.com>
2017-10-17 23:47:34 -0700

    Add before each to restart database.

--- part-2/test/database_test.js
+++ part-2/test/database_test.js
@@ -1,14 +1,11 @@
 const expect = require('chai').assert
 const mocha = require('mocha')
+const { resetDB } = require('./dbUtilities')
 const {getActiveShoppers, shoppersOrders, productList, database} = require('../database/database')
 
 describe("active shopper query", function(){
-  beforeEach((done) => {
-     database.query('DROP TABLE orders')
-     .then(done())
-     .catch(console.error)
-    })
-describe('when given a task', function(){
+  beforeEach('reset the db', resetDB)
+  describe('when given a task', function() {
   it("it will post task to database", function(done){
     getActiveShoppers()
     .then(function(data){
