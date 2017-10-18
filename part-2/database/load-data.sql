f32ddf7ce6a1d5ce916ace346a46f6c278c9220c
Mateo Balcorta <misterrattail@gmail.com>
2017-10-18 11:08:30 -0700

    Add command to copy csv file to database.

--- part-2/database/load-data.sql
+++ part-2/database/load-data.sql
@@ -1,14 +1,7 @@
-
 INSERT INTO shoppers(name) VALUES('Jenny'),('Sebastian'),('Heather'),('Sal'),('OJ'),('Sunshine');
 
 INSERT INTO orders(shopperid) VALUES(1),(1),(4),(4),(3),(3),(3);
 
-INSERT INTO grocery_items(itemname, price, section) VALUES
-('Apples', 2.99, 'produce'),
-('Bacon',9.01,'meat'),
-('Barbeque Sauce',16.96,'packaged'),
-('Carrots',2.88,'produce'),
-('Cheese',1.75,'dairy'),
-('Coffee',6.17,'packaged');
+COPY grocery_items(itemname, price, section) FROM '/Users/mateobalcorta/Desktop/coding/phase2/phase-3-challenge/part-2/database/grocery.csv' DELIMITER ',' CSV HEADER;
 
 INSERT INTO orderitems(orderid, groceryitem) VALUES(1, 1),(1, 2),(1, 1),(2, 4),(3, 6),(4, 4),(4, 5), (5, 5), (6, 2), (7, 3);
