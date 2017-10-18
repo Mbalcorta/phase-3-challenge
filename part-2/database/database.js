8fc1a664dd96d625961910f69e5643574f59e277
Mateo Balcorta <misterrattail@gmail.com>
2017-10-18 09:16:36 -0700

    Change object keys of database return values.

--- part-2/database/database.js
+++ part-2/database/database.js
@@ -8,15 +8,15 @@ const connection = process.env.NODE_ENV !== 'test'
 const database = pgp(connection)
 
 const getActiveShoppers = () => {
-  return database.any('SELECT count(o.shopperid) AS orders, s.name FROM orders o JOIN shoppers s ON(s.shopperid = o.shopperid) GROUP BY o.shopperid, s.name')
+  return database.any('SELECT s.name AS shopper_name, count(o.shopperid) AS number_of_orders  FROM orders o JOIN shoppers s ON(s.shopperid = o.shopperid) GROUP BY o.shopperid, s.name')
 }
 
 const shoppersOrders = (param) => {
-  return database.any(`SELECT i.orderid, SUM(g.price) AS total_cost FROM orders o JOIN orderitems i ON(i.orderid = o.orderid) JOIN grocery_items g ON(g.itemid = i.groceryitem)WHERE o.shopperid = ${param} GROUP BY i.orderid`)
+  return database.any(`SELECT i.orderid AS order_id, SUM(g.price) AS total_cost FROM orders o JOIN orderitems i ON(i.orderid = o.orderid) JOIN grocery_items g ON(g.itemid = i.groceryitem)WHERE o.shopperid = ${param} GROUP BY i.orderid`)
 }
 
 const productList = (param) => {
-  return database.any(`SELECT i.itemname, i.section FROM grocery_items i WHERE section='${param}'`)
+  return database.any(`SELECT i.itemname AS product_name, i.section FROM grocery_items i WHERE section='${param}'`)
 }
 
 module.exports = {getActiveShoppers, shoppersOrders, productList, database}
