99ccc73883417f6b3c2418e09c8c74d4e652f12e
Mateo Balcorta <misterrattail@gmail.com>
2017-10-17 22:12:40 -0700

    Rearrange loading order of data to tables.

--- part-2/database/load-data.sql
+++ part-2/database/load-data.sql
@@ -1,8 +1,7 @@
+
 INSERT INTO shoppers(name) VALUES('Jenny'),('Sebastian'),('Heather'),('Sal'),('OJ'),('Sunshine');
 
 INSERT INTO orders(shopperid) VALUES(1),(1),(4),(4),(3),(3),(3);
-
-INSERT INTO orderitems(orderid, groceryitem) VALUES(1, 1),(1, 2),(1, 1),(2, 4),(3, 6),(4, 4),(4, 5), (5, 5), (6, 2), (7, 3);
 
 INSERT INTO grocery_items(itemname, price, section) VALUES
 ('apples', 2.99, 'produce'),
@@ -11,3 +10,5 @@ INSERT INTO grocery_items(itemname, price, section) VALUES
 ('Carrots',2.88,'produce'),
 ('Cheese',1.75,'dairy'),
 ('Coffee',6.17,'packaged');
+
+INSERT INTO orderitems(orderid, groceryitem) VALUES(1, 1),(1, 2),(1, 1),(2, 4),(3, 6),(4, 4),(4, 5), (5, 5), (6, 2), (7, 3);
