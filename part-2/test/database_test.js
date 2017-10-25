cec20b557f972668ac6862926f6bf2f759c6e855
Mateo Balcorta <misterrattail@gmail.com>
2017-10-24 17:43:42 -0700

    Add testing for product list function.

--- part-2/test/database_test.js
+++ part-2/test/database_test.js
@@ -4,8 +4,6 @@ const {resetDB, addOneOrder, addOneItem, addOneShopper} = require('./dbUtilities
 const {getActiveShoppers, shoppersOrders, productList} = require('../database/database')
 
 describe('Grocery Store query functions', function(){
-//   beforeEach(() => resetDB()
-// console.log('first'))
 beforeEach(resetDB)
 
   describe('Active shoppers function', function(){
@@ -42,6 +40,31 @@ beforeEach(resetDB)
     })
   })
   describe('Product list function', function(){
-
+    describe('When a request is made for a specific type of product', function(){
+      context('Such as dairy item', function(){
+        it('It will return only items that belong to that products category', function(){
+          return productList('dairy')
+          .then(function(data){
+            assert.equal(data.length, 1)
+          })
+        })
+      })
+      context('Such as packaged item', function(){
+        it('It will return only items that belong to that products category', function(){
+          return productList('packaged')
+          .then(function(data){
+            assert.equal(data.length, 2)
+          })
+        })
+      })
+    })
+    describe('When product type does not exist', function(){
+      it('It will not return any items', function(){
+        return productList('candies')
+        .then(function(data){
+          assert.equal(data.length, 0)
+        })
+      })
+    })
   })
 })
