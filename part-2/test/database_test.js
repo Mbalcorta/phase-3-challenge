b5f0761849853fa561b40027f3408abcc0738a52
Mateo Balcorta <misterrattail@gmail.com>
2017-10-24 17:22:47 -0700

    Add test for activeShoppers all test passing.

--- part-2/test/database_test.js
+++ part-2/test/database_test.js
@@ -1,29 +1,47 @@
 const assert = require('chai').assert
 const mocha = require('mocha')
-const {resetDB, addOneOrder, addOneItem} = require('./dbUtilities')
+const {resetDB, addOneOrder, addOneItem, addOneShopper} = require('./dbUtilities')
 const {getActiveShoppers, shoppersOrders, productList} = require('../database/database')
 
 describe('Grocery Store query functions', function(){
-  beforeEach(resetDB)
+//   beforeEach(() => resetDB()
+// console.log('first'))
+beforeEach(resetDB)
 
-  describe('Active shoppers', function(){
+  describe('Active shoppers function', function(){
     it('It will return only users who have orders', function(){
-      return getActiveShoppers()
-      .then(function(data){
-        assert.equal(data.length, 3)
-        })
-        .catch(console.error)
-      })
-  })
-  describe('When a new shopper makes an order', function() {
-    beforeEach('addOrder', addOneItem)
-
-  it('It will return an array of four', function(){
-    // return getActiveShoppers()
-    // .then(function(data){
-    //   assert.deepEqual(data.length, 3)
-    //   })
-    //   .catch(console.error)
+     return getActiveShoppers()
+       .then(function(data){
+         assert.equal(data.length, 3)
+       })
     })
-  });
+    describe('When a new shopper makes an order', function() {
+      beforeEach(() => {
+        addOneOrder(2)
+        .then(() => {
+          addOneItem(8,6)
+        })
+      })
+      it('It will return only users who have orders', function(){
+       return getActiveShoppers()
+       .then(function(data){
+         assert.equal(data.length, 4)
+       })
+      })
+    })
+    describe('When a new shopper is added but does not make an order', function(){
+      beforeEach(()=>{
+        addOneShopper('kiki')
+      })
+      it('It will return only users who have orders', function(){
+        return getActiveShoppers()
+        .then(function(data){
+          assert.equal(data.length, 3)
+        })
+      })
+    })
+  })
+  describe('Product list function', function(){
+
+  })
 })
