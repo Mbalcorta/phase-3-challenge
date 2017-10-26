82a6b2dbbd28bace1c4b51baf4167070928edccb
Mateo Balcorta <misterrattail@gmail.com>
2017-10-26 11:57:02 -0700

    Add tests for shopper orders, all working.

--- part-2/test/database_test.js
+++ part-2/test/database_test.js
@@ -15,9 +15,9 @@ beforeEach(resetDB)
     })
     describe('When a new shopper makes an order', function() {
       beforeEach(() => {
-        addOneOrder(2)
+        return addOneOrder(2)
         .then(() => {
-          addOneItem(8,6)
+          return addOneItem(8,6)
         })
       })
       it('It will return only users who have orders', function(){
@@ -29,13 +29,19 @@ beforeEach(resetDB)
     })
     describe('When a new shopper is added but does not make an order', function(){
       beforeEach(()=>{
-        addOneShopper('kiki')
+        return addOneShopper('kiki')
       })
       it('It will return only users who have orders', function(){
         return getActiveShoppers()
         .then(function(data){
           assert.equal(data.length, 3)
         })
+      })
+    })
+    it('It will return array when active shoppers found', function(){
+      return getActiveShoppers()
+      .then(function(data){
+        assert.isArray(data)
       })
     })
   })
@@ -63,6 +69,55 @@ beforeEach(resetDB)
         return productList('candies')
         .then(function(data){
           assert.equal(data.length, 0)
+        })
+      })
+    })
+    it('It will return array when product type is found', function(){
+      return productList('packaged')
+      .then(function(data){
+        assert.isArray(data)
+      })
+    })
+  })
+  describe('Shoppers orders function', function(){
+    describe('When user has orderItems', function(){
+      it('It will return two orders for shopper number 4', function(){
+        return shoppersOrders(4)
+        .then(function(data){
+          assert.equal(data.length, 2)
+        })
+      })
+      it('It will contain users first order total cost', function(){
+        return shoppersOrders(4)
+        .then((data)=>{
+          assert.equal(data[0].total_cost, '6.17')
+        })
+      })
+      it('It will return users second order total cost', function(){
+        return shoppersOrders(4)
+        .then(function(data){
+          return data
+        })
+        .then((data)=>{
+          assert.equal(data[1].total_cost, '4.63')
+        })
+      })
+      it('It will return array when shopper has orders', function(){
+        return shoppersOrders(4)
+        .then(function(data){
+          assert.isArray(data)
+        })
+      })
+      it('It will return array with order id key', function(){
+        return shoppersOrders(4)
+        .then(function(data){
+          assert.hasAnyKeys(data[0], 'order_id')
+        })
+      })
+      it('It will return array with total cost key', function(){
+        return shoppersOrders(4)
+        .then(function(data){
+          assert.hasAnyKeys(data[0], 'total_cost')
         })
       })
     })
