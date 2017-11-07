0370a0f1ca0532a2cc0071a2bcd62955a9fbe1ea
Mateo Balcorta <misterrattail@gmail.com>
2017-11-07 13:26:00 -0800

    Remove concatArray variable and passed results directly to resolve function.

--- part-1/js/postFunc.js
+++ part-1/js/postFunc.js
@@ -1,8 +1,7 @@
 const concat = (array1, array2) => {
   return new Promise((resolve, reject) => {
     if(Array.isArray(array1) && Array.isArray(array2)){
-      const concatArray = array1.concat(array2)
-      resolve(concatArray)
+      resolve(array1.concat(array2))
     } else {
       reject({"error": "Input data should be of type Array."})
     }
