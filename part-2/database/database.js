435831a02758658432aaf20bb25f95bcf44abf03
Mateo Balcorta <misterrattail@gmail.com>
2017-11-07 12:27:41 -0800

    Change variable name for sql statements to sql convention.

--- part-2/database/database.js
+++ part-2/database/database.js
@@ -1,21 +1,18 @@
 const pgp = require('pg-promise')()
-
-const connection = process.env.NODE_ENV !== 'test'
-  ? 'postgres://localhost:5432/grocery_store'
-  : 'postgres://localhost:5432/grocery_store_test'
-
+const db = process.env.NODE_ENV !== 'test' ? 'grocery_store' : 'grocery_store_test'
+const connection = `postgres://localhost:5432/${db}`
 const database = pgp(connection)
 
 const getActiveShoppers = () => {
-  return database.any('SELECT s.name AS shopper_name, count(o.shopperid) AS number_of_orders  FROM orders o JOIN shoppers s ON(s.shopperid = o.shopperid) GROUP BY o.shopperid, s.name')
+  return database.any('SELECT s.name AS shopper_name, count(o.shopper_id ) AS number_of_orders  FROM orders o JOIN shoppers s ON(s.id  = o.shopper_id) GROUP BY o.shopper_id, s.name')
 }
 
 const shoppersOrders = (param) => {
-  return database.any(`SELECT i.orderid AS order_id, SUM(g.price) AS total_cost FROM orders o JOIN orderitems i ON(i.orderid = o.orderid) JOIN grocery_items g ON(g.itemid = i.groceryitem)WHERE o.shopperid = ${param} GROUP BY i.orderid`)
+  return database.any(`SELECT i.orders_id AS order_id, SUM(g.price) AS total_cost FROM orders o JOIN orders_items i ON(i.orders_id = o.id) JOIN items g ON(g.id = i.items_id)WHERE o.shopper_id = ${param} GROUP BY i.orders_id ORDER BY i.orders_id`)
 }
 
 const productList = (param) => {
-  return database.any(`SELECT i.itemname AS product_name, i.section FROM grocery_items i WHERE section='${param}'`)
+  return database.any(`SELECT i.name AS product_name, i.section FROM items i WHERE section='${param}'`)
 }
 
 module.exports = {getActiveShoppers, shoppersOrders, productList, database}
