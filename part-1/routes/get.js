6efe4738c4f8f806c8ac4cf417e30eb5a7809ebb
Mateo Balcorta <misterrattail@gmail.com>
2017-11-07 10:13:03 -0800

    Change variable query to day also deconstructed day variable.

--- part-1/routes/get.js
+++ part-1/routes/get.js
@@ -2,15 +2,16 @@ const router = require('express').Router()
 const dayValue = require('../js/getFunc')
 
 router.get('/api/days/:day', function (request, response) {
-  const query = request.params.day
-  dayValue(query)
-  .then(data => {
-    response.set('Content-Type', 'text/plain')
-    response.status(200).send((data).toString())
-  })
-  .catch((error) => {
-    response.status(400).send(error)
-  })
+  const { day } = request.params
+
+  dayValue(day)
+    .then(data => {
+      response.set('Content-Type', 'text/plain')
+      response.status(200).send((data).toString())
+    })
+    .catch((error) => {
+      response.status(400).send(error)
+    })
 })
 
 module.exports = router
