c122d3ca008ca8986539b0cf5d4bd869642c59c3
Mateo Balcorta <misterrattail@gmail.com>
2017-11-07 10:10:02 -0800

    Change variable parameter from query to day and change index position of day for resolve function parameter.

--- part-1/js/getFunc.js
+++ part-1/js/getFunc.js
@@ -1,17 +1,13 @@
  const daysOfWeek = {
-   monday: 1, tuesday:2, wednesday: 3, thursday: 4, friday: 5, saturday: 6, sunday: 7
+   monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6, sunday: 7
  }
 
- const dayValue = (query) => {
+ const dayValue = (day) => {
    return new Promise((resolve, reject) => {
-     if(daysOfWeek.hasOwnProperty(query)){
-       for(var day in daysOfWeek) {
-         if(day === query) {
-           resolve(daysOfWeek[day])
-         }
-       }
+     if(daysOfWeek.hasOwnProperty(day)){
+      resolve(daysOfWeek[day])
      }
-     reject(`'${query}' is not a valid day!`)
+     reject(`'${day}' is not a valid day!`)
    })
   }
 
