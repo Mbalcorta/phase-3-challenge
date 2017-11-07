7f22d3c4666e0fb8cd45ada7738e80b908faf678
Mateo Balcorta <misterrattail@gmail.com>
2017-11-07 12:28:17 -0800

    Change variable name for sql statements to sql convention.

--- part-2/database/load-data.sql
+++ part-2/database/load-data.sql
@@ -1,7 +1,7 @@
 INSERT INTO shoppers(name) VALUES('Jenny'),('Sebastian'),('Heather'),('Sal'),('OJ'),('Sunshine');
 
-INSERT INTO orders(shopperid) VALUES(1),(1),(4),(4),(3),(3),(3);
+INSERT INTO orders(shopper_id) VALUES(1),(1),(4),(4),(3),(3),(3);
 
-COPY grocery_items(itemname, price, section) FROM '/Users/mateobalcorta/Desktop/coding/phase2/phase-3-challenge/part-2/database/grocery.csv' DELIMITER ',' CSV HEADER;
+COPY items(name, price, section) FROM '/Users/mateobalcorta/Desktop/coding/phase2/phase-3-challenge/part-2/database/grocery.csv' DELIMITER ',' CSV HEADER;
 
-INSERT INTO orderitems(orderid, groceryitem) VALUES(1, 1),(1, 2),(1, 1),(2, 4),(3, 6),(4, 4),(4, 5), (5, 5), (6, 2), (7, 3);
+INSERT INTO orders_items(orders_id, items_id) VALUES(1, 1),(1, 2),(1, 1),(2, 4),(3, 6),(4, 4),(4, 5), (5, 5), (6, 2), (7, 3);
