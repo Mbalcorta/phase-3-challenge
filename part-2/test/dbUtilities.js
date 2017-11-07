f13810e66877d249339938c828e40a6c7552a2e3
Mateo Balcorta <misterrattail@gmail.com>
2017-11-07 12:32:57 -0800

    Change name of variables to match sql conventions.

--- part-2/test/dbUtilities.js
+++ part-2/test/dbUtilities.js
@@ -1,7 +1,7 @@
 const {database} = require('../database/database.js')
 
 const initDB = () => {
-  const tables = ['grocery_items', 'shoppers', 'orderitems', 'orders']
+  const tables = ['items', 'shoppers', 'orders_items', 'orders']
   return Promise.all(tables.map(table =>
       database.none(`TRUNCATE TABLE ${table} RESTART IDENTITY CASCADE`)
     ))
@@ -20,7 +20,7 @@ const seedShoppers = () => {
 }
 
 const seedOrders = () => {
-  const sql='INSERT INTO orders(shopperid) VALUES($1)';
+  const sql='INSERT INTO orders(shopper_id) VALUES($1)';
   return database.none(sql, 1)
   .then(()=> database.none(sql, 1))
   .then(()=> database.none(sql, 4))
@@ -33,48 +33,50 @@ const seedOrders = () => {
 
 const seedGroceryItems = () => {
   //first need to insert each item one at a time. once each item is included keep doing .then
-  const sql = 'INSERT INTO grocery_items(itemname, price, section) VALUES(${itemname}, ${price}, ${section})'
-  return database.none(sql, {itemname: 'Apples',price: 2.99, section: 'produce'})
-  .then(()=> database.none(sql, {itemname: 'Bacon',price: 9.01, section: 'meat'}))
-  .then(()=> database.none(sql, {itemname: 'Barbeque Sauce',price: 16.96, section: 'packaged'}))
-  .then(()=> database.none(sql, {itemname: 'Carrots',price: 2.88, section: 'produce'}))
-  .then(()=> database.none(sql, {itemname: 'Cheese',price: 1.75, section: 'dairy'}))
-  .then(()=> database.none(sql, {itemname: 'Coffee',price: 6.17, section: 'packaged'}))
+  const sql = 'INSERT INTO items(name, price, section) VALUES(${name}, ${price}, ${section})'
+  return database.none(sql, {name: 'Apples',price: 2.99, section: 'produce'})
+  .then(()=> database.none(sql, {name: 'Bacon',price: 9.01, section: 'meat'}))
+  .then(()=> database.none(sql, {name: 'Barbeque Sauce',price: 16.96, section: 'packaged'}))
+  .then(()=> database.none(sql, {name: 'Carrots',price: 2.88, section: 'produce'}))
+  .then(()=> database.none(sql, {name: 'Cheese',price: 1.75, section: 'dairy'}))
+  .then(()=> database.none(sql, {name: 'Coffee',price: 6.17, section: 'packaged'}))
   .catch(console.error)
 
 }
 
 const seedOrderItems = () => {
   const orderItems =
-  [{orderID: 1, groceryItem: 1},
-  {orderID: 1, groceryItem: 2},
-  {orderID: 1, groceryItem: 1},
-  {orderID: 2, groceryItem: 4},
-  {orderID: 3, groceryItem: 6},
-  {orderID: 4, groceryItem: 4},
-  {orderID: 4, groceryItem: 5},
-  {orderID: 5, groceryItem: 5},
-  {orderID: 6, groceryItem: 2},
-  {orderID: 7, groceryItem: 3}]
+  [{orders_id: 1, items_id: 1},
+  {orders_id: 1, items_id: 2},
+  {orders_id: 1, items_id: 1},
+  {orders_id: 2, items_id: 4},
+  {orders_id: 3, items_id: 6},
+  {orders_id: 4, items_id: 4},
+  {orders_id: 4, items_id: 5},
+  {orders_id: 5, items_id: 5},
+  {orders_id: 6, items_id: 2},
+  {orders_id: 7, items_id: 3}]
   return Promise.all(orderItems.map(item =>
-    database.none('INSERT INTO orderitems(orderID, groceryItem) VALUES(${orderID}, ${groceryItem})', item))
+    database.none('INSERT INTO orders_items(orders_id, items_id) VALUES(${orders_id}, ${items_id})', item))
   ).catch(console.error)
 }
 
 const resetDB = () =>
   initDB()
-  .then(seedShoppers)
-  .then(seedOrders)
-  .then(seedGroceryItems)
-  .then(seedOrderItems)
+.then(seedShoppers)
+.then(seedOrders)
+.then(seedGroceryItems)
+.then(seedOrderItems)
 .catch(console.error)
 
-const addOneOrder = (shopperId) =>
-database.none('INSERT INTO orders(shopperid) VALUES($1)', [shopperId])
+
+
+const addOneOrder = (shopper_id) =>
+database.none('INSERT INTO orders(shopper_id) VALUES($1)', [shopper_id])
 .catch(console.error)
 
-const addOneItem = (orderId, groceryItem) =>
-database.none('INSERT INTO orderitems(orderID, groceryItem) VALUES($1, $2)', [orderId, groceryItem])
+const addOneItem = (orders_id, items_id) =>
+database.none('INSERT INTO orders_items(orders_id, items_id) VALUES($1, $2)', [orders_id, items_id])
 .catch(console.error)
 
 const addOneShopper = (name) =>
