60ba7956db3935f94a0fd602c15880136889020d
Mateo Balcorta <misterrattail@gmail.com>
2017-11-07 10:14:32 -0800

    Deconstructed request body array variables and passed in two variables to concat.

--- part-1/routes/post.js
+++ part-1/routes/post.js
@@ -2,16 +2,17 @@ const router = require('express').Router()
 const concat = require('../js/postFunc')
 
 router.post('/api/array/concat', function (request, response) {
-  const requestObject = request.body
-  concat(requestObject)
-  .then(concatArray => {
-    response.set('Content-Type', 'application/json')
-    response.status(200).send(concatArray)
-  })
-  .catch((error) => {
-    response.set('Content-Type', 'application/json')
-    response.status(400).send(error)
-  })
+  const { array1, array2 } = request.body
+
+  concat(array1, array2)
+    .then(concatArray => {
+      response.set('Content-Type', 'application/json')
+      response.status(200).json({'result': concatArray})
+    })
+    .catch((error) => {
+      response.set('Content-Type', 'application/json')
+      response.status(400).json(error)
+    })
 })
 
 module.exports = router
