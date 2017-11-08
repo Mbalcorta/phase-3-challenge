522fbae47b998436d36bdabb19c8fd709ac57238
Mateo Balcorta <misterrattail@gmail.com>
2017-11-07 21:29:58 -0800

    Change parameter passed into function.

--- part-2/database/database.js
+++ part-2/database/database.js
@@ -7,12 +7,12 @@ const getActiveShoppers = () => {
   return database.any('SELECT s.name AS shopper_name, count(o.shopper_id ) AS number_of_orders  FROM orders o JOIN shoppers s ON(s.id  = o.shopper_id) GROUP BY o.shopper_id, s.name')
 }
 
-const shoppersOrders = (param) => {
-  return database.any(`SELECT i.orders_id AS order_id, SUM(g.price) AS total_cost FROM orders o JOIN orders_items i ON(i.orders_id = o.id) JOIN items g ON(g.id = i.items_id)WHERE o.shopper_id = ${param} GROUP BY i.orders_id ORDER BY i.orders_id`)
+const shoppersOrders = (shopper_id) => {
+  return database.any(`SELECT i.orders_id AS order_id, SUM(g.price) AS total_cost FROM orders o JOIN orders_items i ON(i.orders_id = o.id) JOIN items g ON(g.id = i.items_id)WHERE o.shopper_id = ${shopper_id} GROUP BY i.orders_id ORDER BY i.orders_id`)
 }
 
-const productList = (param) => {
-  return database.any(`SELECT i.name AS product_name, i.section FROM items i WHERE section='${param}'`)
+const productList = (section_name) => {
+  return database.any(`SELECT i.name AS product_name, i.section FROM items i WHERE section='${section_name}'`)
 }
 
 module.exports = {getActiveShoppers, shoppersOrders, productList, database}
